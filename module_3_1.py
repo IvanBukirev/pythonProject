73192eb4400caffe0ca5be63e5c1c03c01a82473
Ivan Bukirev <i.bukirev@gmail.com>
2024-08-22 00:18:40 +0300

    ver2

--- module_3_1.py
+++ module_3_1.py
@@ -1,21 +1,27 @@
-calls =0
+calls = 0
+
+
 def count_calls():
     global calls
     calls += 1
 
+
 def string_info(string):
-    s = (len(string),string.upper(), string.lower())
+    s = (len(string), string.upper(), string.lower())
     count_calls()
     return s
 
 
-def is_contains(string,list_to_search):
+def is_contains(string, list_to_search):
     count_calls()
+    string = string.lower()
+    list_to_search = [s.lower() for s in list_to_search]
     return any(item in string for item in list_to_search)
-
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
 print(is_contains('cycle', ['recycling', 'cyclic']))
+print(string_info('аБРкАДАбра'))
+print(is_contains('Студент', ['СТУдень', 'СтУДент', 'СТУДЕНТЫ']))
 print(calls)
