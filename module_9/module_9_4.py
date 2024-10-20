6ed35f94445dba0e921727cd5d9f670ccc35176b
Ivan Bukirev <i.bukirev@gmail.com>
2024-10-21 01:48:05 +0300

    9_4

--- module_9/module_9_4.py
+++ module_9/module_9_4.py
@@ -1,5 +1,3 @@
-import random
-
 # Lambda-функция:
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
@@ -12,7 +10,7 @@ print(letter_comparison_list)
 def get_advanced_writer(file_name):
 
     def write_everything(*data_set):
-        with open(file_name, 'w', encoding='utf-8') as file:
+        with open(file_name, 'a', encoding='utf-8') as file:
             for item in data_set:
                 file.write("%s\n" % item)
                 # print(item, file=file) или так
@@ -24,12 +22,14 @@ write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
 
 # Метод __call__:
+from random import choice
+
 class MysticBall:
     def __init__(self, *words):
         self.words = words
 
     def __call__(self):
-        return random.choice(self.words)
+        return choice(self.words)
 
 first_ball = MysticBall('Да', 'Нет', 'Наверное')
 print(first_ball())
