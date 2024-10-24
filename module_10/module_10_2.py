a1443befc02e815003ae782973ef3dc103389886
Ivan Bukirev <i.bukirev@gmail.com>
2024-10-25 00:50:57 +0300

    10_2

--- module_10/module_10_2.py
+++ module_10/module_10_2.py
@@ -16,15 +16,15 @@ class Knight(Thread):
         while power_enemy>0:
             count_knight += 1
             power_enemy -= self.power
-            print(f"{self.name}  сражается {count_knight} день(дня)..., осталось {power_enemy} воинов.")
+            print(f"{self.name}  сражается {count_knight} день(дня)..., осталось {power_enemy if power_enemy>0 else 0} воинов.")
             sleep(1)
         print(f"{self.name} одержал победу спустя {count_knight} дней(дня)!")
 
 
 
 
-first_knight = Knight('Sir Lancelot', 10)
-second_knight = Knight("Sir Galahad", 20)
+first_knight = Knight('Sir Lancelot', 35)
+second_knight = Knight("Sir Galahad", 17)
 first_knight.start()
 second_knight.start()
 first_knight.join()
